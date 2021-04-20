ea3642e8a0e0b17eaef6b1a25be630de4dd1e8f7
David E. Flores <floresdavide0@gmail.com>
2021-04-20 08:56:31 -0600

    Se agrego un formulario

--- guia1/ejemplos/cookie1.php
+++ guia1/ejemplos/cookie1.php
@@ -4,9 +4,17 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cookie 1</title>
+    <title>Cookies</title>
 </head>
 <body>
-    
+    <h1>Ejemplo de uso de cookies</h1>
+    <hr>
+    <form action="cookie2.php" method="post">
+        <label for="nombre">Nombre</label><br>
+        <input type="text" name="nombre" id="nombre" required><br>
+        <label for="valor">Valor</label><br>
+        <input type="text" name="valor" id="valor" required><hr>
+        <input type="submit" value="Enviar">
+    </form>
 </body>
 </html>
