8063a1cd1c8de7447098d3cebb3446dede94500a
David E. Flores <floresdavide0@gmail.com>
2021-04-22 08:45:35 -0600

    Se cambio el tiempo de la cookie

--- guia1/ejemplos/cookie2.php
+++ guia1/ejemplos/cookie2.php
@@ -1,14 +1,33 @@
+<?php
+
+    if(isset($_POST["nombre"]) && isset($_POST["valor"])){
+
+        $nombre = $_POST["nombre"];
+        $valor = $_POST["valor"];
+        $tiempo_expi = time() + 5;
+        $ruta = "/";
+
+        setcookie($nombre, $valor, $tiempo_expi, $ruta, "");
+    }else{
+
+        header("Location: cookie1.php");
+    }
+
+    
+?>
 <!DOCTYPE html>
 <html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cookies</title>
+    <title>Cookies dos</title>
 </head>
 <body>
     <h1>Ejemplo de uso de cookies</h1>
     <hr>
     
+    <p>Se ah creado una cookie de nombre: <b><?php echo $_COOKIE[$nombre]; ?></b></p>
+    <p>Con el valor de: <b><?php echo $_COOKIE[$valor]; ?></b></p>
 </body>
 </html>
