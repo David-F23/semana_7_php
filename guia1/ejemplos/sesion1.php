83e6180af7c7636c1781bf55cbd0ba2d63c5833a
David E. Flores <floresdavide0@gmail.com>
2021-04-22 09:13:39 -0600

    Se el archivo 'sesion1. php' se agrego el cuerpo HTML y un formulario

---
+++ guia1/ejemplos/sesion1.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <h1>Sesiones con PHP</h1>
+    <hr>
+    <form action="sesion2.php" method="post">
+        <label for="user">Usuario</label><br>
+        <input type="text" name="user" id="user"><br>
+        <label for="clave">Clave</label><br>
+        <input type="password" name="clave" id="clave"><hr>
+        <input type="submit" value="Iniciar Sesión">
+    </form>
+</body>
+</html>
