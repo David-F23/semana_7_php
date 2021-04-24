e1f89f541bc2625e74d3f92cdacbadd9722958d3
David E. Flores <floresdavide0@gmail.com>
2021-04-24 14:52:27 -0600

    Se imprimieron en pantalla los valores de la sesión

--- guia1/Ejercicios/sesion.php
+++ guia1/Ejercicios/sesion.php
@@ -3,9 +3,13 @@
 
         session_start();
 
-        $_SESSION["SesionUsuario"] = $_POST["usuario"];
+        $_SESSION["SesionUsuario"] = array(
+            'Usuario' => $_POST['usuario'],
+            'Email' => $_POST['email'],
+            'Clave' => $_POST['clave'],
+        ); 
 
-        echo 'Ha iniciado sesión, Bienvenido/a: '. $_SESSION["SesionUsuario"];
+        
     }else{
 
         header('Location: ejercicio2.php');
@@ -19,8 +23,28 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sesión</title>
+
+    <style>
+        h1{
+            text-align: center;
+            border:1px solid;
+        }
+
+        h3{
+            text-align: center;
+        }
+
+        .green{
+            background-color:rgb(0, 255, 0);
+        }
+    </style>
 </head>
 <body>
-    <h1></h1>
+    <h1>Bienvenido/a</h1>
+    <h3><mark class="green"><?php echo 'Ha iniciado sesión: </mark><mark>'. $_SESSION["SesionUsuario"]['Usuario']; ?></mark></h3>
+    <h3><mark class="green"><?php echo 'Correo: </mark><mark>'. $_SESSION["SesionUsuario"]['Email']; ?></mark></h3>
+    <h3><mark class="green"><?php echo 'Clave: </mark><mark>'. $_SESSION["SesionUsuario"]['Clave']; ?></mark></h3>
+
+    
 </body>
 </html>
